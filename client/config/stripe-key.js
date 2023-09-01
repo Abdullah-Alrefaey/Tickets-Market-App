f1e560cf86200b7b153f470eceded8e0920329a1
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2023-09-01 23:30:53 +0300

    updates

--- client/config/stripe-key.js
+++ client/config/stripe-key.js
@@ -1,2 +1,1 @@
-export const STRIPE_PUBLISHABLE_KEY =
-    'pk_test_51Nl6WXASEtP5B0vGGDiYCcoOh0jVLOnOJkyqheBUyc5UYo3nr3E9EKnPlkkRjFHOyvprg1isyewAYYPcnq2qoLtA00On6TguPv';
+export const STRIPE_PUBLISHABLE_KEY = '';
